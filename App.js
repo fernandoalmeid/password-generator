b9b231283d3ef29ae2c5aa9bd1b31348273ea4d9
Fernando Almeida <104784951+fernandoalmeid@users.noreply.github.com>
2025-09-03 02:17:30 +0100

    Update App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import PasswordGenerator from "./passwordgenerator";
+
+function App() {
+    return (
+        <div>
+            <PasswordGenerator />
+        </div>
+    );
+}
+
+export default App;
