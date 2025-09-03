7a17b69aa7c37da352720a1d95041addc826b9a8
Fernando Almeida <104784951+fernandoalmeid@users.noreply.github.com>
2025-09-03 02:17:17 +0100

    Update passwordgenerator.jsx

--- passwordgenerator.jsx
+++ passwordgenerator.jsx
@@ -4,9 +4,10 @@ const PasswordGenerator = () => {
     const [length, setLenght] = useState(12);
     const [includeUpper, setIncludeUpper] = useState(true);
     const [includeLower, setIncludeLower] = useState(true);
-    const [includeNumbers, setIncludeUNumbers] = useState(true);
+    const [includeNumbers, setIncludeNumbers] = useState(true);
     const [includeSymbols, setIncludeSymbols] = useState(true);
     const [password, setPassword] = useState("");
+    const [strength, setStrength] = useState("");
 
     const upperCase = "ABCDEFGHIJKLMONOPQRSTUVXYZ";
     const lowerCase = "abcdefghijklmnopqrstuvxyz";
@@ -23,6 +24,7 @@ const PasswordGenerator = () => {
 
         if (!chars) {
             setPassword("Select at least on option");
+            setStrength("")
             return;
         }
 
@@ -30,15 +32,85 @@ const PasswordGenerator = () => {
         for (let i = 0; i < length; i++) {
             const randomIndex = Math.floor(Math.random() * chars.length);
             generated += chars[randomIndex];
-        }
+        }v
         setPassword(generated);
+        checkStrength(generated)
     };
 
-    const copuToClipoboard = () => {
+    const checkStrength = (pwd) => {
+        let score = 0;
+        if(pwd.length >= 8) score ++;
+        if(pwd.length >= 12) score ++;
+        if(/[A-Z]/.test(pwd)) score++;
+        if(/[a-z]/.test(pwd)) score++;
+        if(/\d/.test(pwd)) score++;
+        if(/[^A-Za-z0-9]/.test(pwd)) score++;
+
+        if (score <= 2) {
+            setStrength("Weak");
+        }else if (score <= 4) {
+            setStrength("Medium");
+        } else {
+            setStrength("Strong");
+        }
+    };
+
+    const copyToClipoboard = () => {
         navigator.clipboard.writeText(password);
         alert("Password copied!");
     };
 
+    return (
+        <div style={{ padding: "20px", maxWidth: "400px", margin: "auto"}}>
 
+            <h2>Password generator</h2>
 
-}
+            <label>
+                length: {length}
+                <input type="range" min="4" max="32" value={length} onChange={(e) => setLength(e.target.value)} />
+            </label>
+
+            <div>
+                <label >
+                    <input type="checkbox" checked={includeUpper} onChange={() => setIncludeUpper(!includeUpper)} /> Uppercase
+                </label>
+                <label>
+                    <input type="checkbox" checked={includeLower} onChange={() => setIncludeLower(!includeLower)} /> Lowercase
+                </label>
+                <label>
+                    <input type="checkbox" checked={includeNumbers} onChange={() => setIncludeNumbers(!includeNumbers)} /> Numbers
+                </label>
+                <label>
+                    <input type="checkbox" checked={includeSymbols} onChange={() => setIncludeSymbols(!includeSymbols)} /> Symbols
+                </label>
+            </div>
+
+            <button onClick={generatePassword}>Generate</button>
+            <div style={{ marginTop: "10px"}}>
+                <input type="text" value={password} readOnly />
+                <button onClick={copyToClipoboard}>Copy</button>
+            </div>
+
+            {strength &&(
+                <p>
+                    strength:{" "}
+                    <span
+                     style={{
+                        color:
+                         strength === " Weak"
+                         ? "red"
+                         : strength === "Medium"
+                         ? "orange"
+                         : "green",
+                    fontWeight: "bold",
+                         }}
+                    >
+                        {strength}
+                    </span>
+                </p>
+            )}
+        </div>
+    );
+};
+
+export default PasswordGenerator;
